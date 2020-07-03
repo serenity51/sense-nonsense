b57c77a50816e1021e0ce4448978b78ff5651206
tranquility <66564457+serenity51@users.noreply.github.com>
2020-07-03 22:18:03 +0530

    Update tryout.py

--- tryout.py
+++ tryout.py
@@ -3,37 +3,20 @@ a = ['orangeade', 'lemonade', 'applejuice','grapejuice']
 b = ['gin', 'vodka', 'whisky']
 
 for drink in drinks:
- if drink == 'gin': break
- print('i do not like', 'gin')
+ if drink == 'gin':
+     break
+ print('i do not like gin but like', drink)
 
 for drink in drinks:
  if drink == 'orangeade': continue
  print(drink)
 
 drink =input('enter type of drink\t')
-if a: print('that is okay-carry on')
-else: print('be careful-you may get drunk')
+for a in drink :
+    if a:
+     print('okay-enjoy your soft drink' )
 
-# Please look at the output--looks okay except for the last bit --else not working.
-# let me know--no hurry--lv msg anytime you are free.
-#output for the first for loop
-# i do not like gin
-# i do not like gin
-# i do not like gin
-# i do not like gin
-
-# output for the 2nd for loop
-# lemonade
-# applejuice
-# grapejuice
-# gin
-# vodka
-
-#output after entering drink
-# enter type of drink a
-# that is okay-carry on   #  this is the correct output
-
-# enter type of drink  b
-# that is okay-carry on     # this is the wrong output -getting same output for a and b
-
-
+drink =input('enter type of drink\t')
+for b in drink:
+    if b:
+     print('be careful- you may get drunk')
